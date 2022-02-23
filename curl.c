1ac1ea725c9f5bb8cd8fdf44e5686c99effa29f0
LucasDatilioCarderelli <llucascarderelli@gmail.com>
2022-02-23 16:46:55 -0300

    server response

---
+++ curl.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ldatilio <ldatilio@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/23 13:01:48 by ldatilio          #+#    #+#             */
+/*   Updated: 2022/02/23 15:28:23 by ldatilio         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <curl/curl.h>
+#include <unistd.h>
+
+int main(void)
+{
+    CURL *curl = curl_easy_init();
+
+    if (!curl)
+    {
+        fprintf(stderr,"[-] Failed Initializing Curl\n");
+        exit(-1);
+    }
+	//https://www.mercadobitcoin.net/api/BTC/ticker/
+    CURLcode res;
+    curl_easy_setopt(curl, CURLOPT_URL, "localhost:8000/6");
+    res = curl_easy_perform(curl);
+
+    if (res != CURLE_OK)
+    {
+        fprintf(stderr,"[-] Could Not Fetch Webpage\n[+] Error : %s\n",curl_easy_strerror(res));
+        exit(-2);
+    }
+
+    curl_easy_cleanup(curl);
+    return 0;
+}
