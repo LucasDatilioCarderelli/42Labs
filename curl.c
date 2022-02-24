59dae1219dd30bb8a1a7286ffdc21ff42ade004c
LucasDatilioCarderelli <llucascarderelli@gmail.com>
2022-02-23 22:53:59 -0300

    cleaning for norminette

--- curl.c
+++ curl.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   curl.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ldatilio <ldatilio@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/23 13:01:48 by ldatilio          #+#    #+#             */
-/*   Updated: 2022/02/23 15:28:23 by ldatilio         ###   ########.fr       */
+/*   Updated: 2022/02/23 16:56:06 by ldatilio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,26 +15,27 @@
 #include <curl/curl.h>
 #include <unistd.h>
 
-int main(void)
+int	main(void)
 {
-    CURL *curl = curl_easy_init();
+	CURL		*curl;
+	CURLcode	res;
 
-    if (!curl)
-    {
-        fprintf(stderr,"[-] Failed Initializing Curl\n");
-        exit(-1);
-    }
-	//https://www.mercadobitcoin.net/api/BTC/ticker/
-    CURLcode res;
-    curl_easy_setopt(curl, CURLOPT_URL, "localhost:8000/6");
-    res = curl_easy_perform(curl);
-
-    if (res != CURLE_OK)
-    {
-        fprintf(stderr,"[-] Could Not Fetch Webpage\n[+] Error : %s\n",curl_easy_strerror(res));
-        exit(-2);
-    }
-
-    curl_easy_cleanup(curl);
-    return 0;
+	curl = curl_easy_init();
+	if (!curl)
+	{
+		fprintf(stderr, "[-] Failed Initializing Curl\n");
+		exit(-1);
+	}
+	curl_easy_setopt(curl, CURLOPT_URL, "localhost:8000/6");
+	res = curl_easy_perform(curl);
+	if (res != CURLE_OK)
+	{
+		fprintf(
+			stderr,
+			"[-] Could Not Fetch Webpage\n[+] Error : %s\n",
+			curl_easy_strerror(res));
+		exit(-2);
+	}
+	curl_easy_cleanup(curl);
+	return (0);
 }
--- server.c
+++ server.c
@@ -6,71 +6,77 @@
 /*   By: ldatilio <ldatilio@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/22 11:48:59 by ldatilio          #+#    #+#             */
-/*   Updated: 2022/02/23 16:26:45 by ldatilio         ###   ########.fr       */
+/*   Updated: 2022/02/23 17:13:53 by ldatilio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./mongoose/mongoose.h"
 
-void log_message(const char *filename, const char *message, int status)
+void	log_message(const char *filename, const char *message, int status)
 {
-    time_t curtime;
-    FILE *fp = fopen(filename, "a");
+	time_t	curtime;
+	FILE	*fp;
 
-    time(&curtime);
-    if (fp != NULL)
-    {
-        fprintf(fp, "Status: %d\n", status);
-        fprintf(fp, "Ultima requisição: %s", ctime(&curtime));
-        fprintf(fp, "%s", message);
-        fclose(fp);
-    }
+	fp = fopen(filename, "a");
+	time(&curtime);
+	if (fp != NULL)
+	{
+		fprintf(fp, "Status: %d\n", status);
+		fprintf(fp, "Last Request: %s", ctime(&curtime));
+		fprintf(fp, "%s", message);
+		fclose(fp);
+	}
 }
 
-static void fn(struct mg_connection *c, int ev, void *ev_data, void *fn_data)
+static void	fn(struct mg_connection *c, int ev, void *ev_data, void *fn_data)
 {
-    int aux;
-    if (ev == MG_EV_HTTP_MSG)
-    {
-        struct mg_http_message *hm = (struct mg_http_message *)ev_data;
-        if (mg_http_match_uri(hm, "/4"))
-        {
-            aux = rand() % 4;
-            mg_http_reply(c, 200, "dado de 4 lados\r\n", "{\"valor\":%d}", aux);
-			log_message("LOG.log", hm->message.ptr, 200);
-        }
-        else if(mg_http_match_uri(hm, "/6"))
-        {
-            aux = rand() % 6;
-            mg_http_reply(c, 200, "dado de 6 lados\r\n", "{\"valor\":%d}", aux);
-			log_message("LOG.log", hm->message.ptr, 200);
-        }
-         else if(mg_http_match_uri(hm, "/10"))
-        {
-            aux = rand() % 10;
-            mg_http_reply(c, 200, "dado de 10 lados\r\n", "{\"valor\":%d}", aux);
-			log_message("LOG.log", hm->message.ptr, 200);
-        }
-         else if(mg_http_match_uri(hm, "/20"))
-        {
-            aux = rand() % 20;
-            mg_http_reply(c, 200, "dado de 20 lados\r\n", "{\"valor\":%d}", aux);
-			log_message("LOG.log", hm->message.ptr, 200);
-        }
-        else
+	struct mg_http_message	*hm;
+	int						aux;
+
+	hm = (struct mg_http_message *)ev_data;
+	if (ev == MG_EV_HTTP_MSG)
+	{
+		if (mg_http_match_uri(hm, "/4"))
 		{
-            mg_http_reply(c, 404, NULL, "Dado não encontrado\n");
+			aux = rand() % 4;
+			mg_http_reply(c, 200, "dado de 4 lados\r\n", "{\"valor\":%d}", aux);
+			log_message("LOG.log", hm->message.ptr, 200);
+		}
+		else if (mg_http_match_uri(hm, "/6"))
+		{
+			aux = rand() % 6;
+			mg_http_reply(c, 200, "dado de 6 lados\r\n", "{\"valor\":%d}", aux);
+			log_message("LOG.log", hm->message.ptr, 200);
+		}
+		else if (mg_http_match_uri(hm, "/10"))
+		{
+			aux = rand() % 10;
+			mg_http_reply(c, 200, "dado de 10 lados\r\n", "{\"valor\":%d}", aux);
+			log_message("LOG.log", hm->message.ptr, 200);
+		}
+		else if (mg_http_match_uri(hm, "/20"))
+		{
+			aux = rand() % 20;
+			mg_http_reply(c, 200, "dado de 20 lados\r\n", "{\"valor\":%d}", aux);
+			log_message("LOG.log", hm->message.ptr, 200);
+		}
+		else
+		{
+			mg_http_reply(c, 404, NULL, "Dado não encontrado\n");
 			log_message("LOG.log", hm->message.ptr, 404);
 		}
-    }
+	}
 }
 
-int main(int argc, char *argv[]) {
-  struct mg_mgr mgr;
-  mg_log_set("4");
-  mg_mgr_init(&mgr);                                        // Init manager
-  mg_http_listen(&mgr, "http://localhost:8000", fn, &mgr);  // Setup listener
-  for (;;) mg_mgr_poll(&mgr, 1000);                         // Event loop
-  mg_mgr_free(&mgr);                                        // Cleanup
-  return 0;
+int	main(int argc, char *argv[])
+{
+	struct mg_mgr	mgr;
+
+	mg_log_set("4");
+	mg_mgr_init(&mgr);
+	mg_http_listen(&mgr, "http://localhost:8000", fn, &mgr);
+	while (1)
+		mg_mgr_poll(&mgr, 2000);
+	mg_mgr_free(&mgr);
+	return (0);
 }
