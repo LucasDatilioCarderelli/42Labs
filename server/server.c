995a349e9e73cdbb963621947c5b1e3589e23a1d
LucasDatilioCarderelli <llucascarderelli@gmail.com>
2022-02-25 16:39:23 -0300

    Update routes

--- server/server.c
+++ server/server.c
@@ -6,35 +6,43 @@
 /*   By: ldatilio <ldatilio@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/22 11:48:59 by ldatilio          #+#    #+#             */
-/*   Updated: 2022/02/25 10:21:55 by ldatilio         ###   ########.fr       */
+/*   Updated: 2022/02/25 15:44:33 by ldatilio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "server.h"
 
+static void	change_route(struct mg_connection *c, struct mg_http_message *hm,
+						char *route)
+{
+	char	*body;
+
+	body = ft_curl(route);
+	mg_http_reply(c, 200, "Coin Status\r\n", "%s\n", body);
+	log_message(LOG_FILE, hm->message.ptr, 200);
+}
+
 static void	fn(struct mg_connection *c, int ev, void *ev_data, void *fn_data)
 {
 	struct mg_http_message	*hm;
-	char					*body;
 
 	hm = (struct mg_http_message *)ev_data;
 	if (ev == MG_EV_HTTP_MSG)
 	{
-		if (mg_http_match_uri(hm, "/BTC"))
+		if (mg_http_match_uri(hm, "/"))
 		{
-			body = ft_curl("BTC");
-			mg_http_reply(c, 200, "BTC\r\n", "%s\n", body);
+			mg_http_reply(c, 200, "HomePage\r\n",
+				"Choose a coin, try BTC or ETH\n");
 			log_message(LOG_FILE, hm->message.ptr, 200);
 		}
+		else if (mg_http_match_uri(hm, "/BTC"))
+			change_route(c, hm, "BTC");
 		else if (mg_http_match_uri(hm, "/ETH"))
-		{
-			body = ft_curl("ETH");
-			mg_http_reply(c, 200, "ETH\r\n", "%s\n", body);
-			log_message(LOG_FILE, hm->message.ptr, 200);
-		}
+			change_route(c, hm, "ETH");
 		else
 		{
-			mg_http_reply(c, 404, NULL, "Coin not found, try BTC or ETH\n");
+			mg_http_reply(c, 404, "Page not Found",
+				"Coin not found, try BTC or ETH\r\n");
 			log_message(LOG_FILE, hm->message.ptr, 404);
 		}
 	}
@@ -44,7 +52,7 @@ int	main(void)
 {
 	struct mg_mgr	mgr;
 
-	mg_log_set("4");
+	mg_log_set("3");
 	mg_mgr_init(&mgr);
 	mg_http_listen(&mgr, "http://localhost:8000", fn, &mgr);
 	while (1)
