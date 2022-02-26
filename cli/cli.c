93a9b2d82558d478067d4f24439afd74e8154953
LucasDatilioCarderelli <llucascarderelli@gmail.com>
2022-02-25 21:09:41 -0300

    add news coins

--- cli/cli.c
+++ cli/cli.c
@@ -6,11 +6,62 @@
 /*   By: ldatilio <ldatilio@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/23 19:37:45 by ldatilio          #+#    #+#             */
-/*   Updated: 2022/02/25 16:32:28 by ldatilio         ###   ########.fr       */
+/*   Updated: 2022/02/25 20:54:20 by ldatilio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cli.h"
+
+static void	init_variables(t_data *method);
+static void	count_requests(t_data *method, int fd);
+static void	print_cli(t_data *method);
+
+int	main(void)
+{
+	t_data	method;
+	int		fd;
+
+	init_variables(&method);
+	fd = open(LOG_FILE, O_RDONLY);
+	if (fd == -1)
+		printf("Error");
+	count_requests(&method, fd);
+	print_cli(&method);
+}
+
+static void	init_variables(t_data *method)
+{
+	method->get.root = 0;
+	method->get.btc = 0;
+	method->get.eth = 0;
+	method->get.bat = 0;
+	method->get.gods = 0;
+	method->error = 0;
+}
+
+static void	count_requests(t_data *method, int fd)
+{
+	char	*tmp_line;
+
+	while (1)
+	{
+		tmp_line = get_next_line(fd);
+		if (tmp_line == NULL)
+			break ;
+		if (strstr(tmp_line, "GET / "))
+			method->get.root++;
+		if (strstr(tmp_line, "GET /BTC"))
+			method->get.btc++;
+		if (strstr(tmp_line, "GET /ETH"))
+			method->get.eth++;
+		if (strstr(tmp_line, "GET /BAT"))
+			method->get.bat++;
+		if (strstr(tmp_line, "GET /GODS"))
+			method->get.gods++;
+		if (strstr(tmp_line, "Status: 404"))
+			method->error++;
+	}
+}
 
 static void	print_cli(t_data *method)
 {
@@ -26,32 +77,14 @@ static void	print_cli(t_data *method)
 	if (method->get.eth)
 		printf("║      %02d      ║      GET      ║      /ETH     ║\n",
 			method->get.eth);
+	if (method->get.bat)
+		printf("║      %02d      ║      GET      ║      /BAT     ║\n",
+			method->get.bat);
+	if (method->get.gods)
+		printf("║      %02d      ║      GET      ║      /GODS    ║\n",
+			method->get.gods);
+	if (method->error)
+		printf("║      %02d      ║     ERROR     ║       404     ║\n",
+			method->error);
 	printf("╚══════════════╩═══════════════╩═══════════════╝\n");
-}
-
-int	main(void)
-{
-	t_data	method;
-	int		fd;
-	char	*tmp_line;
-
-	method.get.root = 0;
-	method.get.btc = 0;
-	method.get.eth = 0;
-	fd = open(LOG_FILE, O_RDONLY);
-	if (fd == -1)
-		printf("Error.");
-	while (1)
-	{
-		tmp_line = get_next_line(fd);
-		if (tmp_line == NULL)
-			break ;
-		if (strstr(tmp_line, "GET / "))
-			method.get.root++;
-		if (strstr(tmp_line, "GET /BTC"))
-			method.get.btc++;
-		if (strstr(tmp_line, "GET /ETH"))
-			method.get.eth++;
-	}
-	print_cli(&method);
 }
--- server/server.c
+++ server/server.c
@@ -6,47 +6,15 @@
 /*   By: ldatilio <ldatilio@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/22 11:48:59 by ldatilio          #+#    #+#             */
-/*   Updated: 2022/02/25 15:44:33 by ldatilio         ###   ########.fr       */
+/*   Updated: 2022/02/25 20:30:44 by ldatilio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "server.h"
 
-static void	change_route(struct mg_connection *c, struct mg_http_message *hm,
-						char *route)
-{
-	char	*body;
-
-	body = ft_curl(route);
-	mg_http_reply(c, 200, "Coin Status\r\n", "%s\n", body);
-	log_message(LOG_FILE, hm->message.ptr, 200);
-}
-
-static void	fn(struct mg_connection *c, int ev, void *ev_data, void *fn_data)
-{
-	struct mg_http_message	*hm;
-
-	hm = (struct mg_http_message *)ev_data;
-	if (ev == MG_EV_HTTP_MSG)
-	{
-		if (mg_http_match_uri(hm, "/"))
-		{
-			mg_http_reply(c, 200, "HomePage\r\n",
-				"Choose a coin, try BTC or ETH\n");
-			log_message(LOG_FILE, hm->message.ptr, 200);
-		}
-		else if (mg_http_match_uri(hm, "/BTC"))
-			change_route(c, hm, "BTC");
-		else if (mg_http_match_uri(hm, "/ETH"))
-			change_route(c, hm, "ETH");
-		else
-		{
-			mg_http_reply(c, 404, "Page not Found",
-				"Coin not found, try BTC or ETH\r\n");
-			log_message(LOG_FILE, hm->message.ptr, 404);
-		}
-	}
-}
+static void
+change_route(struct mg_connection *c, struct mg_http_message *hm, char *route);
+static void	fn(struct mg_connection *c, int ev, void *ev_data, void *fn_data);
 
 int	main(void)
 {
@@ -59,4 +27,43 @@ int	main(void)
 		mg_mgr_poll(&mgr, 2000);
 	mg_mgr_free(&mgr);
 	return (0);
+}
+
+static void	fn(struct mg_connection *c, int ev, void *ev_data, void *fn_data)
+{
+	struct mg_http_message	*hm;
+
+	hm = (struct mg_http_message *)ev_data;
+	if (ev == MG_EV_HTTP_MSG)
+	{
+		if (mg_http_match_uri(hm, "/"))
+		{
+			mg_http_reply(c, 200, "HomePage\r\n",
+				"{\"CoinsAvailables\":[\"BTC\", \"ETH\", \"BAT\", \"GODS\"]}");
+			log_message(LOG_FILE, hm->message.ptr, 200);
+		}
+		else if (mg_http_match_uri(hm, "/BTC"))
+			change_route(c, hm, "BTC");
+		else if (mg_http_match_uri(hm, "/ETH"))
+			change_route(c, hm, "ETH");
+		else if (mg_http_match_uri(hm, "/BAT"))
+			change_route(c, hm, "BAT");
+		else if (mg_http_match_uri(hm, "/GODS"))
+			change_route(c, hm, "GODS");
+		else
+		{
+			mg_http_reply(c, 404, NULL, "Page Not found, try BTC or ETH\r\n");
+			log_message(LOG_FILE, hm->message.ptr, 404);
+		}
+	}
+}
+
+static void	change_route(
+	struct mg_connection *c, struct mg_http_message *hm, char *route)
+{
+	char	*body;
+
+	body = ft_curl(route);
+	mg_http_reply(c, 200, "Coin Status\r\n", "%s\n", body);
+	log_message(LOG_FILE, hm->message.ptr, 200);
 }
