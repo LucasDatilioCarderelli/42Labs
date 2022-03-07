8d87ad8c1dd28e248b38c6661c11883563e3b627
LucasDatilioCarderelli <llucascarderelli@gmail.com>
2022-03-07 09:09:43 -0300

    change db to g_db

--- server/parse_json.c
+++ server/parse_json.c
@@ -6,14 +6,14 @@
 /*   By: ldatilio <ldatilio@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/04 17:40:07 by ldatilio          #+#    #+#             */
-/*   Updated: 2022/03/06 19:25:18 by ldatilio         ###   ########.fr       */
+/*   Updated: 2022/03/07 09:02:59 by ldatilio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "server.h"
 
 /* Create SQL statement */
-static void	create_table(sqlite3 *db)
+static void	create_table(void)
 {
 	char	*zerrmsg;
 	char	*sql;
@@ -25,7 +25,7 @@ static void	create_table(sqlite3 *db)
 		"DATE		DATETIME," \
 		"SELL		CHAR(20)," \
 		"BUY		CHAR(20) );";
-	rc = sqlite3_exec(db, sql, NULL, 0, &zerrmsg);
+	rc = sqlite3_exec(g_db, sql, NULL, 0, &zerrmsg);
 	if (rc != SQLITE_OK)
 	{
 		fprintf(stderr, "SQL error: %s\n", zerrmsg);
@@ -37,9 +37,25 @@ static void	create_table(sqlite3 *db)
 	}
 }
 
+void	*open_db(sqlite3 *db)
+{
+	int		rc;
+
+	rc = sqlite3_open("database.db", db);
+	if (rc)
+	{
+		fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+		return (0);
+	}
+	else
+	{
+		fprintf(stderr, "Opened database successfully\n");
+	}
+	create_table();
+}
+
 /* Create SQL statement */
-static void	insert_db(sqlite3 *db,
-const char *date, const char *sell, const char *buy)
+static void	insert_db(const char *date, const char *sell, const char *buy)
 {
 	char	*zerrmsg;
 	char	*query;
@@ -50,7 +66,7 @@ const char *date, const char *sell, const char *buy)
 			"INSERT INTO COINBOOK (DATE, SELL, BUY)	\
 			VALUES ('%q', '%q','%q');",
 			date, sell, buy);
-	rc = sqlite3_exec(db, query, NULL, 0, &zerrmsg);
+	rc = sqlite3_exec(g_db, query, NULL, 0, &zerrmsg);
 	if (rc != SQLITE_OK)
 	{
 		fprintf(stderr, "SQL error: %s\n", zerrmsg);
@@ -60,31 +76,10 @@ const char *date, const char *sell, const char *buy)
 	{
 		fprintf(stdout, "Records created successfully\n");
 	}
-	sqlite3_close(db);
-}
-
-static sqlite3	*open_db(void)
-{
-	sqlite3	*db;
-	int		rc;
-
-	rc = sqlite3_open("database.db", &db);
-	if (rc)
-	{
-		fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
-		return (0);
-	}
-	else
-	{
-		fprintf(stderr, "Opened database successfully\n");
-	}
-	create_table(db);
-	return (db);
 }
 
 void	parse_json(char *body)
 {
-	sqlite3				*db;
 	struct json_object	*parsed_json;
 	struct json_object	*ticker;
 	struct json_object	*date;
@@ -96,9 +91,7 @@ void	parse_json(char *body)
 	json_object_object_get_ex(ticker, "date", &date);
 	json_object_object_get_ex(ticker, "sell", &sell);
 	json_object_object_get_ex(ticker, "buy", &buy);
-	db = open_db();
 	insert_db(
-		db,
 		json_object_get_string(date),
 		json_object_get_string(sell),
 		json_object_get_string(buy)
